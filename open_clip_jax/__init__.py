1e51bd767b9b84e39ce49e3f754f38fff2155d47
Borna Ahmadzadeh <borna.ahz@gmail.com>
2023-05-19 11:08:21 -0400

    Modify __init__ imports

--- open_clip_jax/__init__.py
+++ open_clip_jax/__init__.py
@@ -10,15 +10,7 @@ tf.config.set_visible_devices([], 'GPU')
 
 
 from .clip import (
-    CLIP,
     CLIPInference,
-    CLIPWithLoss,
-    IMAGENET_DATASET_MEAN,
-    IMAGENET_DATASET_STD,
-    INCEPTION_DATASET_MEAN,
-    INCEPTION_DATASET_STD,
-    OPENAI_DATASET_MEAN,
-    OPENAI_DATASET_STD,
     create_model,
     create_model_with_params,
     image_transforms,
@@ -27,10 +19,4 @@ from .clip import (
     list_pretrained_by_model,
     preprocess_image,
     tokenize,
-    )
-from .training import (
-    create_csv_dataset,
-    create_learning_rate_scheduler,
-    create_weight_decay_mask,
-    train_and_validate,
     )
--- open_clip_jax/training/__init__.py
+++ open_clip_jax/training/__init__.py
@@ -6,4 +6,4 @@ Codebase for training CLIP models.
 from .data import create_csv_dataset
 from .optimizer import create_weight_decay_mask
 from .scheduler import create_learning_rate_scheduler
-from .train import train_and_validate
+from .train import TrainState, train_and_validate
