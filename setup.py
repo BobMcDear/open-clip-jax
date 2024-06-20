f03d9881ee24a53e49b34576a5c7d843c66aa9ed
Borna Ahmadzadeh <borna.ahz@gmail.com>
2024-06-20 10:49:04 -0400

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name='open_clip_jax',
-	version='0.0.1',
+	version='0.0.2',
 	description='Implementation of CLIP in JAX/Flax',
 	author='Borna Ahmadzadeh',
 	author_email='borna.ahz@gmail.com',
